c606e65d53bd501691495c0eb4c0bb0411bd7a2f
rzhang15 <rzhang15@exseed.ed.ac.uk>
2020-09-18 19:59:29 +0100

    update stacking fault

--- HighEntropyCeramics/stacking_fault.py
+++ HighEntropyCeramics/stacking_fault.py
@@ -2,7 +2,7 @@
 import os
 import numpy as np
 import math
-from pymatgen import Structure
+from pymatgen import Structure, Lattice
 from pymatgen.io.vasp.inputs import Poscar
 
 from CrystalToolkit.composition import solid_solution_random
@@ -28,62 +28,67 @@ slip_systems = [
     },
 ]
 
-compo_list = [
-    'Ti', 'Zr', 'Nb', 'Ta',
-#    'Ti-Zr-Nb-Ta',
-]
+anions = ['N']
+cations = {
+    'Ti': 4.238, 
+    'Zr': 4.622, 
+    'Nb': 4.444, 
+    'Ta': 4.410,
+#    'Ti-Zr-Nb-Ta': 4.428,
+}
+
 a2 = 0.0
-
-for compo in compo_list:  
-    stru_file = compo + 'N.cif'
-    compo_name = compo + '-N'
-
-    for slip_sys in slip_systems:
-        hkl, a1vect_uvw, a2vect_uvw = slip_sys.values()
-
-        elem_list = []
-        a1_max, nstep = 0.25, 11
-        nstep = 11
-
-        if hkl == [1,1,1]:
+for anion in anions:
+    for cation in cations.keys():  
+        for slip_sys in slip_systems:
+            hkl, a1vect_uvw, a2vect_uvw = slip_sys.values()
             sizemults = [1, 1, 4]
-            if a1vect_uvw == [1,1,-2]:
-                a1_max, nstep = 0.5, 21
-        elif hkl == [1,1,0]:
-            sizemults = [1, 1, 6]
 
-        if len(compo) > 2:
-            elem_frac_site = 'Ti'
-            elem_list = compo.split('-')
-            sizemults = [x1 * x2 for x1, x2 in zip(sizemults, [2,2,1])]
-            a1_max, nstep = 2, 21
+            for a1 in np.linspace(0, a1_max, nstep):
+                # every time stru should be built
+                # otherwise it seems to be changed by the subroutine 
+                # probably consider to use istructrue?
+                if len(cation) < 3:
+                    a1_max, nstep = 0.25, 11
+                    if a1vect_uvw == [1, 1, -2]:
+                        a1_max, nstep = 0.5, 21
+                    stru = Structure.from_spacegroup('Fm-3m', Lattice.cubic(cations[cation]),
+                                                [cation, anion],[[0.5, 0.5, 0.5], [0, 0, 0]])
+                    stru_fault = stacking_fault_generation(structure=stru, sizemults=sizemults, 
+                                                    a1=a1, a2=a2, hkl=hkl, a1vect_uvw=a1vect_uvw, 
+                                                    a2vect_uvw=a2vect_uvw)
+                else:
+                    sizemults = [x1 * x2 for x1, x2 in zip(sizemults, [2,2,1])]
+                    a1_max, nstep = 2, 21
+                    hec_elems = cation.split('-')
+                    # pymatgen seems not accept fraction in the from_spacegroup method
+                    # only this can be done stru['X'] = 'X0.5Y0.5', so use hec_elems[0]
+                    stru = Structure.from_spacegroup('Fm-3m', Lattice.cubic(cations[cation]),
+                                                [hec_elems[0], anion],[[0.5, 0.5, 0.5], [0, 0, 0]])
+                    stru_fault = stacking_fault_generation(structure=stru, sizemults=sizemults, 
+                                                    a1=a1, a2=a2, hkl=hkl, a1vect_uvw=a1vect_uvw, 
+                                                    a2vect_uvw=a2vect_uvw)
+                    stru_fault = solid_solution_random(structure=stru_fault, 
+                                                        elem_frac_site=hec_elems[0], 
+                                                        elem_list=hec_elems)
 
-        for a1 in np.linspace(0, a1_max, nstep):
-            # every time stru should be read, otherwise it seems to be changed by the subroutine
-            stru = Structure.from_file(stru_file)
-            stru_fault = stacking_fault_generation(structure=stru, sizemults=sizemults, a1=a1, a2=a2, 
-                                                    hkl=hkl, a1vect_uvw=a1vect_uvw, a2vect_uvw=a2vect_uvw)
-            if elem_list:
-                stru_fault_solidsolu = solid_solution_random(structure=stru_fault, 
-                                                    elem_frac_site=elem_frac_site, elem_list=elem_list)
-            else:
-                stru_fault_solidsolu = stru_fault
-                stru_fault_solidsolu['Ti'] = compo
+                stru_fault.sort()
+                selective_dynamics = poscar_fix_top_bottom(stru_fault)
+                fault_sys_poscar = Poscar(structure=stru_fault, 
+                                            selective_dynamics=selective_dynamics)
 
-            selective_dynamics = poscar_fix_top_bottom(stru_fault_solidsolu)
-            fault_sys_poscar = Poscar(structure=stru_fault_solidsolu, selective_dynamics=selective_dynamics)
-            fault_sys_poscar.sort()
+                compo_name = cation + '-' + anion
+                if math.isclose(a1, 0.0):
+                    dir = compo_name \
+                            + '_'.join([''.join(map(str, hkl)), 'plane', 
+                                        ''.join(map(str, a1vect_uvw))]) \
+                            + '_' + format(a1, '.2f')
+                else:
+                    dir = compo_name \
+                            + '_'.join([''.join(map(str, hkl)), 'plane'])
 
-            if math.isclose(a1, 0.0):
-                dir = compo_name \
-                        + '_'.join([''.join(map(str, hkl)), 'plane', ''.join(map(str, a1vect_uvw))]) \
-                        + '_' + format(a1, '.2f')
-            else:
-                dir = compo_name \
-                        + '_'.join([''.join(map(str, hkl)), 'plane'])
-
-            if not os.path.isdir(dir):
-                os.mkdir(dir)
-                fault_sys_poscar.write_file(dir + '/POSCAR', ) 
+                if not os.path.isdir(dir):
+                    os.mkdir(dir)
+                    fault_sys_poscar.write_file(dir + '/POSCAR', ) 
 
 
